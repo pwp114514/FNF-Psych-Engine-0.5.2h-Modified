f14416a43bfdecb0e34ac4b1295d2f71183e3a43
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-09-16 19:07:16 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1334,6 +1334,95 @@ class PlayState extends MusicBeatState
 		}
 		#end
 	}
+	public function addShaderToCamera(cam:String,effect:ShaderEffect){//STOLE FROM ANDROMEDA
+	  
+	  
+	  
+			switch(cam.toLowerCase()) {
+				case 'camhud' | 'hud':
+						camHUDShaders.push(effect);
+						var newCamEffects:Array<BitmapFilter>=[]; // IT SHUTS HAXE UP IDK WHY BUT WHATEVER IDK WHY I CANT JUST ARRAY<SHADERFILTER>
+						for(i in camHUDShaders){
+						  newCamEffects.push(new ShaderFilter(i.shader));
+						}
+						camHUD.setFilters(newCamEffects);
+				case 'camother' | 'other':
+						camOtherShaders.push(effect);
+						var newCamEffects:Array<BitmapFilter>=[]; // IT SHUTS HAXE UP IDK WHY BUT WHATEVER IDK WHY I CANT JUST ARRAY<SHADERFILTER>
+						for(i in camOtherShaders){
+						  newCamEffects.push(new ShaderFilter(i.shader));
+						}
+						camOther.setFilters(newCamEffects);
+				case 'camgame' | 'game':
+						camGameShaders.push(effect);
+						var newCamEffects:Array<BitmapFilter>=[]; // IT SHUTS HAXE UP IDK WHY BUT WHATEVER IDK WHY I CANT JUST ARRAY<SHADERFILTER>
+						for(i in camGameShaders){
+						  newCamEffects.push(new ShaderFilter(i.shader));
+						}
+						camGame.setFilters(newCamEffects);
+				default:
+					if(modchartSprites.exists(cam)) {
+						Reflect.setProperty(modchartSprites.get(cam),"shader",effect.shader);
+					} else if(modchartTexts.exists(cam)) {
+						Reflect.setProperty(modchartTexts.get(cam),"shader",effect.shader);
+					} else {
+						var OBJ = Reflect.getProperty(PlayState.instance,cam);
+						Reflect.setProperty(OBJ,"shader", effect.shader);
+					}	
+		}
+}
+
+public function removeShaderFromCamera(cam:String,effect:ShaderEffect){
+	  
+		switch(cam.toLowerCase()) {
+			case 'camhud' | 'hud': 
+    camHUDShaders.remove(effect);
+    var newCamEffects:Array<BitmapFilter>=[];
+    for(i in camHUDShaders){
+      newCamEffects.push(new ShaderFilter(i.shader));
+    }
+    camHUD.setFilters(newCamEffects);
+			case 'camother' | 'other': 
+					camOtherShaders.remove(effect);
+					var newCamEffects:Array<BitmapFilter>=[];
+					for(i in camOtherShaders){
+					  newCamEffects.push(new ShaderFilter(i.shader));
+					}
+					camOther.setFilters(newCamEffects);
+			default: 
+				camGameShaders.remove(effect);
+				var newCamEffects:Array<BitmapFilter>=[];
+				for(i in camGameShaders){
+				  newCamEffects.push(new ShaderFilter(i.shader));
+				}
+				camGame.setFilters(newCamEffects);
+		}
+		
+	  
+  }
+	
+	
+	
+  public function clearShaderFromCamera(cam:String){
+	  
+	  
+		switch(cam.toLowerCase()) {
+			case 'camhud' | 'hud': 
+				camHUDShaders = [];
+				var newCamEffects:Array<BitmapFilter>=[];
+				camHUD.setFilters(newCamEffects);
+			case 'camother' | 'other': 
+				camOtherShaders = [];
+				var newCamEffects:Array<BitmapFilter>=[];
+				camOther.setFilters(newCamEffects);
+			default: 
+				camGameShaders = [];
+				var newCamEffects:Array<BitmapFilter>=[];
+				camGame.setFilters(newCamEffects);
+		}
+		
+	  
+  }
 	
 	function startCharacterPos(char:Character, ?gfCheck:Bool = false) {
 		if(gfCheck && char.curCharacter.startsWith('gf')) { //IF DAD IS GIRLFRIEND, HE GOES TO HER POSITION
@@ -2640,7 +2729,13 @@ class PlayState extends MusicBeatState
 		setOnLuas('cameraY', camFollowPos.y);
 		setOnLuas('botPlay', cpuControlled);
 		callOnLuas('onUpdatePost', [elapsed]);
+		
+		for (i in shaderUpdates){
+			i(elapsed);
+		}
 	}
+
+		
 
 	function openChartEditor()
 	{
